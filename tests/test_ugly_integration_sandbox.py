11a08b5ccdd820465488710cedae371f8a38991b
meisei4 <lanur622@gmail.com>
2023-05-18 20:04:29 +0900

    start trying to use watchdog, tomorrow your mission is to learn how watchdog works better, also how logging can be improved even more

--- tests/test_ugly_integration_sandbox.py
+++ tests/test_ugly_integration_sandbox.py
@@ -5,31 +5,58 @@ import ankikindle
 import ankiconnect_wrapper
 from .conftest import logger
 from watchdog.observers import Observer
-from watchdog.events import DirCreatedEvent, DirDeletedEvent
+from watchdog.events import FileSystemEventHandler, DirDeletedEvent, DirModifiedEvent
 
 
-# TODO this is the ugly physical mount and monitor the log with your eyes test ewww
-def test_continuous_until_manual_exit_mount_and_unmount_with_db_processing():
+def test_continuous_running_until_manual_exit_mount_and_unmount_with_db_processing():
     watch_for_kindle_mount_nonflask(ankikindle, ankiconnect_wrapper, "/Volumes", "Kindle")
 
 
-def handle_mount_event(ankikindle_injection, ankiconnect_injection, kindle_device_name, mounted, event):
-    if isinstance(event, DirCreatedEvent):
-        return handle_kindle_mount(ankikindle_injection, ankiconnect_injection, kindle_device_name, event)
-    elif isinstance(event, DirDeletedEvent):
-        return handle_kindle_unmount(kindle_device_name, mounted, event)
-    else:
-        return mounted
+def watch_for_kindle_mount_nonflask(ankikindle_injection, ankiconnect_injection, mount_dir_root, kindle_device_name):
+    logger.info('Starting Kindle mounting watcher...')
+    observer = Observer()
+
+    class KindleEventHandler(FileSystemEventHandler):
+        def __init__(self):
+            self.directories = set(os.listdir(mount_dir_root))
+
+        def on_modified(self, event):
+            new_directories = set(os.listdir(mount_dir_root))
+            added = new_directories - self.directories
+            removed = self.directories - new_directories
+
+            for directory in added:
+                if directory == kindle_device_name:
+                    logger.info(f"A new device '{directory}' has been mounted.")
+                    handle_kindle_mount(ankikindle_injection, ankiconnect_injection, mount_dir_root, kindle_device_name)
+
+            for directory in removed:
+                if directory == kindle_device_name:
+                    logger.info(f"The device '{directory}' has been unmounted.")
+                    handle_kindle_unmount(kindle_device_name)
+
+            self.directories = new_directories
+
+    event_handler = KindleEventHandler()
+    observer.schedule(event_handler, mount_dir_root, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.info('KeyboardInterrupt received, stopping observer...')
+        observer.stop()
+
+    observer.join()
 
 
-def handle_kindle_mount(ankikindle_injection, ankiconnect_injection, kindle_device_name, event):
-    if os.path.basename(event.src_path) != kindle_device_name:
-        return False
+def handle_kindle_mount(ankikindle_injection, ankiconnect_injection, mount_dir_root, kindle_device_name):
+    logger.info('Checking for Kindle mount...')
+    vocab_db_path = os.path.join(mount_dir_root, kindle_device_name, 'system', 'vocabulary', 'vocab.db')
+    time.sleep(1)  # Give some time for the filesystem to settle.
 
-    vocab_db_path = os.path.join(event.src_path, 'system', 'vocabulary', 'vocab.db')
-    time.sleep(1)  # allow some time for the filesystem to settle
-
-    for _ in range(10):  # retry a few times in case the DB isn't ready yet
+    for _ in range(10):  # Retry a few times in case the DB isn't ready yet.
         if os.path.exists(vocab_db_path):
             try:
                 connection = sqlite3.connect(vocab_db_path)
@@ -40,33 +67,12 @@ def handle_kindle_mount(ankikindle_injection, ankiconnect_injection, kindle_devi
                     connection.close()
                 return True  # mounted
             except sqlite3.Error as e:
-                logger.error(f"An error occurred: {e.args[0]}")
+                logger.error(f'Error occurred while trying to connect to vocab.db: {e.args[0]}')
         time.sleep(1)
 
+    logger.info('Unable to connect to vocab.db after several attempts.')
     return False  # not mounted
 
 
-def handle_kindle_unmount(kindle_device_name, mounted, event):
-    if os.path.basename(event.src_path) == kindle_device_name and mounted:
-        logger.info(f'{kindle_device_name} unmounted')
-        return False  # not mounted
-
-    return mounted
-
-
-def watch_for_kindle_mount_nonflask(ankikindle_injection, ankiconnect_injection, mount_dir_root, kindle_device_name):
-    observer = Observer()
-    mounted = False
-
-    def event_handler(event):
-        nonlocal mounted
-        mounted = handle_mount_event(ankikindle_injection, ankiconnect_injection, kindle_device_name, mounted, event)
-
-    observer.schedule(event_handler, mount_dir_root, recursive=False)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)  # keep the script running
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+def handle_kindle_unmount(kindle_device_name):
+    logger.info(f'{kindle_device_name} unmounted')
