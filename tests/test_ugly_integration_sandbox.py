925a6f8713c792c17b12ae7db77bd0665b37935f
meisei4 <lanur622@gmail.com>
2023-05-18 20:56:16 +0900

    got the first highlight to work using watchdog and the ugly sandbox, LOGGING IS REALLY IMPORTANT
    more important that debugging with breakpoints

--- tests/test_ugly_integration_sandbox.py
+++ tests/test_ugly_integration_sandbox.py
@@ -1,18 +1,21 @@
 import os
 import time
 import sqlite3
-import ankikindle
 import ankiconnect_wrapper
+from .. import ankikindle # TODO find out why the pycharm allows for the incorrect semantics of "import ankikindle"
 from .conftest import logger
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, DirDeletedEvent, DirModifiedEvent
+from watchdog.events import FileSystemEventHandler
 
 
 def test_continuous_running_until_manual_exit_mount_and_unmount_with_db_processing():
     watch_for_kindle_mount_nonflask(ankikindle, ankiconnect_wrapper, "/Volumes", "Kindle")
 
 
-def watch_for_kindle_mount_nonflask(ankikindle_injection, ankiconnect_injection, mount_dir_root, kindle_device_name):
+def watch_for_kindle_mount_nonflask(ankikindle_injection: ankikindle,
+                                    ankiconnect_injection: ankiconnect_wrapper,
+                                    mount_dir_root: str,
+                                    kindle_device_name: str):
     logger.info('Starting Kindle mounting watcher...')
     observer = Observer()
 
@@ -24,7 +27,6 @@ def watch_for_kindle_mount_nonflask(ankikindle_injection, ankiconnect_injection,
             new_directories = set(os.listdir(mount_dir_root))
             added = new_directories - self.directories
             removed = self.directories - new_directories
-
             for directory in added:
                 if directory == kindle_device_name:
                     logger.info(f"A new device '{directory}' has been mounted.")
@@ -51,11 +53,13 @@ def watch_for_kindle_mount_nonflask(ankikindle_injection, ankiconnect_injection,
     observer.join()
 
 
-def handle_kindle_mount(ankikindle_injection, ankiconnect_injection, mount_dir_root, kindle_device_name):
+def handle_kindle_mount(ankikindle_injection: ankikindle,
+                        ankiconnect_injection: ankiconnect_wrapper,
+                        mount_dir_root: str,
+                        kindle_device_name: str):
     logger.info('Checking for Kindle mount...')
     vocab_db_path = os.path.join(mount_dir_root, kindle_device_name, 'system', 'vocabulary', 'vocab.db')
     time.sleep(1)  # Give some time for the filesystem to settle.
-
     for _ in range(10):  # Retry a few times in case the DB isn't ready yet.
         if os.path.exists(vocab_db_path):
             try:
